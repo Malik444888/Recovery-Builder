cdfd7a0bc47eb2813bd1e2afb34a2bd27120d540
meenapriya36301 <123662639+meenapriya36301@users.noreply.github.com>
2023-09-11 23:25:57 +0530

    Add files via upload

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,10 +1,14 @@
-# Android device tree for qti Kona for arm64 (kona)
-
-```
 #
 # Copyright (C) 2023 The Android Open Source Project
 # Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
 #
 # SPDX-License-Identifier: Apache-2.0
 #
-```
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_kona.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_kona-user \
+    omni_kona-userdebug \
+    omni_kona-eng
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,10 +1,10 @@
-# Android device tree for qti Kona for arm64 (kona)
-
-```
 #
 # Copyright (C) 2023 The Android Open Source Project
 # Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
 #
 # SPDX-License-Identifier: Apache-2.0
 #
-```
+
+add_lunch_combo omni_kona-user
+add_lunch_combo omni_kona-userdebug
+add_lunch_combo omni_kona-eng
